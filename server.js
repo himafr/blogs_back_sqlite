811e93262eb9787b268f8f6894a26260f5fe1497
hima <hemawith4@gmail.com>
2024-07-30 04:30:13 +0300

    sqlite

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const sqlite = require("sqlite3").verbose();
 const db = new sqlite.Database("./blogs.db", sqlite.OPEN_READWRITE, (err) => {
   if (err) console.log(err);
 });
+
 exports.database=db
+
 const PORT = process.env.PORT || 3000;
 
-// connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("listening for requests");
-  });
 
-// });
+app.listen(PORT, () => {
+    console.log("listening for requests...");
+  });
